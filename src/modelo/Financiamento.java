dd4a82a3b5391c34a8eaf7efd967fe3f211cadcc
Vlademir Segan Faria <vlademir.faria@pucpr.edu.br>
2024-08-26 23:50:42 -0300

    opa

--- src/modelo/Financiamento.java
+++ src/modelo/Financiamento.java
@@ -1,5 +1,6 @@
 package modelo;
-
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 public class Financiamento {
     private double valorImovel;
     private int prazoFinanciamento;
@@ -12,27 +13,38 @@ public class Financiamento {
     }
 
     public double getValorImovel(){
-        return valorImovel;
+        return this.valorImovel;
     }
 
     public int getPrazoFinanciamento(){
-        return prazoFinanciamento;
+        return this.prazoFinanciamento;
     }
 
     public double getTaxaJurosAnual(){
-        return taxaJurosAnual;
+        return this.taxaJurosAnual;
     }
 
-    public double calcularPagamentoMensal(){
-        return (this.valorImovel / (this.prazoFinanciamento * 12)) * (1 + (this.taxaJurosAnual / 12));
+    public BigDecimal calcularPagamentoMensal() {
+        BigDecimal taxaJurosMensal = BigDecimal.valueOf(this.taxaJurosAnual).divide(BigDecimal.valueOf(12 * 100), 10, RoundingMode.HALF_UP);
+        int numeroParcelas = this.prazoFinanciamento * 12;
+
+        BigDecimal fator = taxaJurosMensal.add(BigDecimal.ONE).pow(numeroParcelas);
+        BigDecimal pagamentoMensal = BigDecimal.valueOf(this.valorImovel)
+                .multiply(taxaJurosMensal)
+                .multiply(fator)
+                .divide(fator.subtract(BigDecimal.ONE), 2, RoundingMode.HALF_UP);
+
+        return pagamentoMensal;
     }
 
-    public double calcularTotalPagamento(){
-        return this.calcularPagamentoMensal() * this.prazoFinanciamento * 12;
+
+    public BigDecimal calcularTotalPagamento() {
+        BigDecimal numeroParcelas = BigDecimal.valueOf(this.prazoFinanciamento * 12);
+        return calcularPagamentoMensal().multiply(numeroParcelas);
     }
 
     public void dadosFinanciamento(){
-        System.out.println("O valor do imovel é de: R$" + getValorImovel());
+        System.out.printf("O valor do imovel é de: R$%.2f\n", getValorImovel());
         System.out.printf("O prazo do financiamento é de: %d anos.\n", getPrazoFinanciamento());
         System.out.printf("A taxa de juros anual é de: %.2f\n", getTaxaJurosAnual());
         System.out.printf("A taxa de juros mensal é de: %.2f\n", getTaxaJurosAnual() / 12);
--- src/util/InterfaceUsuario.java
+++ src/util/InterfaceUsuario.java
@@ -8,16 +8,30 @@ public class InterfaceUsuario {
         double valorDesejadoImovel = scanner.nextDouble();
         return valorDesejadoImovel;
     }
+
     public int pedirPrazoFinanciamento() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Informe o prazo do financiamento em anos: ");
-        int prazoFinanciamentoAnos = scanner.nextInt();
+        int prazoFinanciamentoAnos;
+        do {
+            System.out.println("Informe o prazo do financiamento em anos: (mínimo de 10 e máximo de 35 anos) ");
+            prazoFinanciamentoAnos = scanner.nextInt();
+            if (prazoFinanciamentoAnos < 10 || prazoFinanciamentoAnos > 35) {
+                System.out.println("Digite um valor válido de anos.");
+            }
+        } while (prazoFinanciamentoAnos < 10 || prazoFinanciamentoAnos > 35);
         return prazoFinanciamentoAnos;
     }
+
     public double pedirTaxaJuros() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Informe a taxa de juros: ");
-        double taxaJurosAnual = scanner.nextDouble();
+        double taxaJurosAnual;
+        do {
+            System.out.println("Informe a taxa de juros: ");
+            taxaJurosAnual = scanner.nextDouble();
+            if (taxaJurosAnual < 8.99 || taxaJurosAnual > 12) {
+                System.out.println("Digite um valor válido de taxas de juros");
+            }
+        } while (taxaJurosAnual < 8.99 || taxaJurosAnual > 12);
         return taxaJurosAnual;
     }
 }
