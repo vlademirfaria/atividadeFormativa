13caf56ef1f9fed4e9318066adbcaee496e10af0
Vlademir Segan Faria <vlademir.faria@pucpr.edu.br>
2024-08-30 23:53:43 -0300

    opa

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,33 @@
 package main;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.math.BigDecimal;
 import modelo.Financiamento;
 import util.InterfaceUsuario;
 
 public class Main {
     public static void main(String[] args) {
         InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
-        double taxaJuros = interfaceUsuario.pedirTaxaJuros();
-        int prazoFinanciamentoEmAnos = interfaceUsuario.pedirPrazoFinanciamento();
-        double valorImovel = interfaceUsuario.pedirValorImovel();
+        List<Financiamento> financiamentos = new ArrayList<>();
 
-        Financiamento novoFinanciamento = new Financiamento(valorImovel, prazoFinanciamentoEmAnos, taxaJuros);
-        novoFinanciamento.dadosFinanciamento();
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("Financiamento " + i + ":");
+            Financiamento financiamento = interfaceUsuario.criarFinanciamento();
+            financiamentos.add(financiamento);
+            // Exibe os detalhes do financiamento
+            financiamento.dadosFinanciamento();
+            System.out.println(); // Linha em branco para melhor leitura
+        }
+
+        double totalImoveis = 0;
+        BigDecimal totalFinanciamentos = BigDecimal.ZERO;
+
+        for (Financiamento f : financiamentos){
+            totalImoveis += f.getValorImovel();
+            totalFinanciamentos = totalFinanciamentos.add(f.calcularTotalPagamento());
+        }
+
+        System.out.printf("Total de todos os imóveis: R$%.2f\n", totalImoveis);
+        System.out.printf("Total de todos os financiamentos: R$%.2f\n", totalFinanciamentos);
     }
 }
--- src/util/InterfaceUsuario.java
+++ src/util/InterfaceUsuario.java
@@ -1,14 +1,23 @@
 package util;
+import modelo.Financiamento;
 import java.util.Scanner;
 
 public class InterfaceUsuario {
+    private Scanner scanner = new Scanner(System.in);
+
+    public Financiamento criarFinanciamento() {
+        double valorImovel = pedirValorImovel();
+        int prazoFinanciamento = pedirPrazoFinanciamento();
+        double taxaJuros = pedirTaxaJuros();
+        return new Financiamento(valorImovel, prazoFinanciamento, taxaJuros);
+    }
+
     public double pedirValorImovel() {
-        Scanner scanner = new Scanner(System.in);
         double valorDesejadoImovel;
         do {
             System.out.println("Informe o valor do imóvel: ");
             valorDesejadoImovel = scanner.nextDouble();
-            if (valorDesejadoImovel < 10000) {
+            if (valorDesejadoImovel < 10000) { // Valor minimo que a maioria dos bancos financiam terrenos/imóveis
                 System.out.println("Valor de financiamento muito baixo. Digite um valor maior.");
             }
         } while (valorDesejadoImovel < 10000);
@@ -16,12 +25,11 @@ public class InterfaceUsuario {
     }
 
     public int pedirPrazoFinanciamento() {
-        Scanner scanner = new Scanner(System.in);
         int prazoFinanciamentoAnos;
         do {
             System.out.println("Informe o prazo do financiamento em anos: (mínimo de 10 e máximo de 35 anos) ");
             prazoFinanciamentoAnos = scanner.nextInt();
-            if (prazoFinanciamentoAnos < 10 || prazoFinanciamentoAnos > 35) {
+            if (prazoFinanciamentoAnos < 10 || prazoFinanciamentoAnos > 35) { // Prazo minímo e máximo que os bancos trabalham
                 System.out.println("Digite um valor válido de anos.");
             }
         } while (prazoFinanciamentoAnos < 10 || prazoFinanciamentoAnos > 35);
@@ -29,15 +37,14 @@ public class InterfaceUsuario {
     }
 
     public double pedirTaxaJuros() {
-        Scanner scanner = new Scanner(System.in);
         double taxaJurosAnual;
         do {
             System.out.println("Informe a taxa de juros: ");
             taxaJurosAnual = scanner.nextDouble();
-            if (taxaJurosAnual < 8.99 || taxaJurosAnual > 12) {
-                System.out.println("Digite um valor válido de taxas de juros");
+            if (taxaJurosAnual < 8.99 || taxaJurosAnual > 12) { // Taxa de juro minima que a maioria dos bancos praticam
+                System.out.println("Digite um valor válido de taxas de juros"); // em projetos como minha casa minha vida
             }
-        } while (taxaJurosAnual < 8.99 || taxaJurosAnual > 12);
-        return taxaJurosAnual;
+        } while (taxaJurosAnual < 8.99 || taxaJurosAnual > 12); // taxa de juros anual máxima praticada pela
+        return taxaJurosAnual;                                  // maioria dos bancos
     }
 }
